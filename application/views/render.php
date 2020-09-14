baa49669c6fbff61d64c30cd0dcd792fb15d9620
prasert.n <prasert.n@quo-global.com>
2020-09-14 10:04:37 +0700

    add print qr

--- application/views/render.php
+++ application/views/render.php
@@ -1,17 +1,29 @@
 <html>
-<title><?php  echo $title; ?></title>
+<head>
+	<title><?php  echo $title; ?></title>
+</head>
+<style>
+
+	@media print {
+		.no-show-print{
+			display:none;
+		}
+	}
+</style>
 <body >
 <h1>QR Code</h1>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <img style="width: 80px" src="<?php echo site_url('render/Qrcode/1234jhasdkjhakjshdkjahskdhakjshdj')?>" alt="">
 
-<form onsubmit="uploadFileExcel(this)" action="<?php echo base_url('render/uploadQr')?>" id="site_upload_uploadData">
+<form class="no-show-print" onsubmit="uploadFileExcel(this)" action="<?php echo base_url('render/uploadQr')?>" id="site_upload_uploadData">
 	<input type="file" class="ui-hidden" id="import_site" name="userfile" size="20">
 	<button type="submit"  class="  bmmodal-button" id="site_upload_checkstatus_import" action="http://cms.kod-d.com/site/import" value="import"><i class="fas fa-file-import"></i> Import</button>
 </form>
+<button class="no-show-print" onclick="window.print()">print</button>
 <div id="render_QR">
 
 </div>
+
 <script>
 	function uploadFileExcel(me){
 
@@ -51,6 +63,21 @@
 				}
 			});
 	}
+
+	async function newImageSrc(src) {
+		// Get a reference to the image in whatever way suits.
+		let image = document.getElementById('image-id');
+
+		// Update the source.
+		img.src = src;
+
+		// Wait for it to load.
+		await new Promise((resolve) => { image.onload = resolve; });
+
+		// Done!
+		console.log('image loaded! do something...');
+	}
 </script>
+
 </body>
 </html>
--- application/views/renderqr.php
+++ application/views/renderqr.php
@@ -2,7 +2,7 @@
 <title><?php  echo $title; ?></title>
 <body >
 
-<img style="width: 120px" src="<?php echo base_url('render/Qrcode/'.$kodenya)?>" alt="">
+<img style="width: 120px" class="qr-code" src="<?php echo base_url('render/Qrcode/'.$kodenya)?>" alt="">
 
 
 </body>
